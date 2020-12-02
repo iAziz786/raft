5c9d26ae98ee518aec9c78d6a8b9c90669105ec0
Mohammad Aziz <mdaziz067@gmail.com>
2020-12-03 00:54:00 +0530

    Move SetKey to different file

--- cmd/raft.go
+++ cmd/raft.go
@@ -1,16 +1,15 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/spf13/cobra"
 	"log"
-	"math"
 	"net"
 	"net/http"
 	"net/rpc"
 	"os"
 	"sync"
+
+	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
@@ -114,63 +113,7 @@ func Run(cmd *cobra.Command, args []string) {
 
 	http.HandleFunc("/value", GetKey)
 
-	http.HandleFunc("/set", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case PUT:
-		case DELETE:
-			break
-		default:
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("unsupported method"))
-			return
-		}
-		var nodesToSendRPC []string
-
-		for _, node := range nodes {
-			if "localhost:"+raftPort != node {
-				nodesToSendRPC = append(nodesToSendRPC, node)
-			}
-		}
-
-		decoder := json.NewDecoder(r.Body)
-
-		var updateKey UpdateKey
-
-		err := decoder.Decode(&updateKey)
-
-		if err != nil {
-			log.Fatalf("failed to decode updateKey")
-		}
-
-		command := "PUT"
-		coords.Log = append(coords.Log, appendLog(coords.Term, command, updateKey.Key, updateKey.Value))
-
-		appendResultChan := CallRemoteNode(coords, nodesToSendRPC, command, updateKey.Key, updateKey.Value)
-
-		successfulAppend := 1
-		for ar := range appendResultChan {
-			if ar.Success == true {
-				successfulAppend++
-			}
-			// calculate that successful send is more than half
-			if math.Ceil(float64(successfulAppend)/2) > float64(len(nodesToSendRPC)/2) {
-				// add the value to the data store
-				replicatedStateMachine[updateKey.Key] = updateKey.Value
-			}
-		}
-
-		response, err := json.Marshal(LogEntry{Key: updateKey.Key, Value: updateKey.Value})
-
-		if err != nil {
-			log.Fatal("unable to marshal response JSON")
-		}
-
-		w.Header().Set("content-type", "application/json")
-		_, err = w.Write(response)
-		if err != nil {
-			log.Println("unable to write the response")
-		}
-	})
+	http.HandleFunc("/set", SetKey(coords))
 
 	if err := http.ListenAndServe(":"+httpPort, nil); err != nil {
 		fmt.Println("error while serving", err)
---
+++ cmd/set.go
@@ -0,0 +1,70 @@
+package cmd
+
+import (
+	"encoding/json"
+	"log"
+	"math"
+	"net/http"
+)
+
+// SetKey will set the value for the respective key
+func SetKey(coords *Coords) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case PUT:
+		case DELETE:
+			break
+		default:
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("unsupported method"))
+			return
+		}
+		var nodesToSendRPC []string
+
+		for _, node := range nodes {
+			if "localhost:"+raftPort != node {
+				nodesToSendRPC = append(nodesToSendRPC, node)
+			}
+		}
+
+		decoder := json.NewDecoder(r.Body)
+
+		var updateKey UpdateKey
+
+		err := decoder.Decode(&updateKey)
+
+		if err != nil {
+			log.Fatalf("failed to decode updateKey")
+		}
+
+		command := "PUT"
+		coords.Log = append(coords.Log, appendLog(coords.Term, command, updateKey.Key, updateKey.Value))
+
+		appendResultChan := CallRemoteNode(coords, nodesToSendRPC, command, updateKey.Key, updateKey.Value)
+
+		successfulAppend := 1
+		for ar := range appendResultChan {
+			if ar.Success == true {
+				successfulAppend++
+			}
+			// calculate that successful send is more than half
+			if math.Ceil(float64(successfulAppend)/2) > float64(len(nodesToSendRPC)/2) {
+				// add the value to the data store
+				replicatedStateMachine[updateKey.Key] = updateKey.Value
+			}
+		}
+
+		response, err := json.Marshal(LogEntry{Key: updateKey.Key, Value: updateKey.Value})
+
+		if err != nil {
+			log.Fatal("unable to marshal response JSON")
+		}
+
+		w.Header().Set("content-type", "application/json")
+		_, err = w.Write(response)
+		if err != nil {
+			log.Println("unable to write the response")
+		}
+
+	}
+}
